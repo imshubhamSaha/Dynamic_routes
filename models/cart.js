a994fc6f05fce998887887379b201cbb7bd9b717
Shubham Saha <imshubhamsaha@gmail.com>
2023-12-29 00:27:38 +0530

    Added Delete Functionality

--- models/cart.js
+++ models/cart.js
@@ -12,7 +12,9 @@ module.exports = class Cart {
     //Fetch the previous Cart
     fs.readFile(p, (err, fileContent) => {
       let cart = { products: [], totalPrice: 0 };
-      if (!err) cart = JSON.parse(fileContent);
+      if (!err) {
+        cart = JSON.parse(fileContent);
+      }
 
       // Analyze the cart => Find existing product
       const existingProductIndex = cart.products.findIndex(
@@ -32,6 +34,25 @@ module.exports = class Cart {
       }
       cart.totalPrice = cart.totalPrice + +productprice;
       fs.writeFile(p, JSON.stringify(cart), (err) => {
+        console.log(err);
+      });
+    });
+  }
+
+  static deleteProduct(id, productPrice) {
+    fs.readFile(p, (err, fileContent) => {
+      if (err) return;
+
+      const updatedCart = { ...JSON.parse(fileContent) };
+      const product = updatedCart.products.find((prod) => prod.id === id);
+      const productQty = product.qty;
+      updatedCart.products = updatedCart.products.filter(
+        (prod) => prod.id !== id
+      );
+      updatedCart.totalPrice =
+        updatedCart.totalPrice - productQty * productPrice;
+
+      fs.writeFile(p, JSON.stringify(updatedCart), (err) => {
         console.log(err);
       });
     });
